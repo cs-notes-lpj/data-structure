cce450c2300cd3b5681673b113e051335b7294ac
Brannua <374453156@qq.com>
2021-10-10 17:32:36 +0800

    定义,声明,初始化一个带头结点的循环双链表

---
+++ 1_linearList/CircularDoubleLinkedList.c
@@ -0,0 +1,40 @@
+/**
+ * 循环双链表
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* --------- 定义循环双链表 --------- */
+struct NODE {
+  int data;
+  struct NODE * next;
+  struct NODE * prior;
+};
+
+typedef struct NODE CDNode;
+typedef struct NODE * CDList;
+/* --------- 定义循环双链表 --------- */
+
+// 初始化一个带头结点的循环双链表
+CDList InitCDList(CDList * list) {
+  CDNode * tmp = (CDNode *)malloc(sizeof(CDNode));
+  if (tmp == NULL) {
+    return NULL;
+  }
+
+  tmp->next = tmp;
+  tmp->prior = tmp;
+  (*list) = tmp;
+
+  return (*list);
+}
+
+int main() {
+
+  // 声明并初始化一个带头结点的循环双链表
+  CDList L;
+  InitCDList(&L);
+
+  return 0;
+}
