b88757e154e7823872ab7375855b050de814be24
Brannua <374453156@qq.com>
2021-10-12 22:39:44 +0800

    bool Push(SeqStack * s, int elem)

--- 2_stack/SeqStack.c
+++ 2_stack/SeqStack.c
@@ -29,6 +29,32 @@ bool isEmpty(SeqStack s) {
   return (s.top == -1);
 }
 
+// 入栈
+bool Push(SeqStack * s, int elem) {
+  // 栈满则拒绝进栈
+  if (s->top == maxSize - 1) {
+    return false;
+  }
+
+  s->data[s->top + 1] = elem;
+  s->top = s->top + 1;
+  return true;
+}
+
+// 从栈顶向栈底遍历输出栈
+void printStack(SeqStack s) {
+  // 栈空则直接返回
+  if (s.top == -1) {
+    return;
+  }
+
+  for (int i = s.top; i >= 0; i --) {
+    printf("%d ", s.data[i]);
+  }
+  printf("\n");
+  return;
+}
+
 int main() {
 
   SeqStack s;
@@ -37,6 +63,11 @@ int main() {
   }
 
   printf("顺序栈变量所占内存字节数：%d\n", sizeof(s)); // 44 => maxSize * sizeof(int) + sizeof(int)
+
+  Push(&s, 111);
+  Push(&s, 222);
+  Push(&s, 333);
+  printStack(s);
 
 
 
