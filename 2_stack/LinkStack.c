ef66b2400c5490d713047c53627e8af53a65b2a3
Brannua <374453156@qq.com>
2021-10-13 09:38:08 +0800

    定义数据类型：链栈的结点

---
+++ 2_stack/LinkStack.c
@@ -0,0 +1,22 @@
+/**
+ * 链栈 <= 单链表 + 只允许在一端进行插入删除操作
+*/
+
+#include <stdio.h>
+
+/* --- 定义数据类型：链栈的结点 --- */
+struct NODE {
+  int data;
+  struct NODE * next;
+};
+
+typedef struct NODE LSNode;
+typedef struct NODE * LinkStack;
+/* --- 定义数据类型：链栈的结点 --- */
+
+int main() {
+
+  LinkStack s = NULL;
+
+  return 0;
+}
