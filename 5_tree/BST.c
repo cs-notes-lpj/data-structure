e25fc90b8dbe91334e8f6f90e5b6beda8127b83d
Brannua <374453156@qq.com>
2021-11-13 20:51:02 +0800

    二叉排序树的基本操作:insert 的非递归实现

--- 5_tree/BST.c
+++ 5_tree/BST.c
@@ -64,6 +64,7 @@ bool insert(BST_Tree * T, char key) {
     tmp->rChild = NULL;
 
     (*T) = tmp;
+    return true;
 
   }
 
@@ -79,6 +80,77 @@ bool insert(BST_Tree * T, char key) {
     BST_Tree rTree = (*T)->rChild;
     return insert(&rTree, key);
   }
+
+}
+
+// 非递归实现（空间复杂度 => O(1)）
+bool insert(BST_Tree * T, char key) {
+
+  // 树空，则直接插入
+  if ((*T) == NULL) {
+
+    BST_Node * tmp = (BST_Node *)malloc(sizeof(BST_Node));
+    if (tmp == NULL) {
+      return false;
+    }
+
+    tmp->data = key;
+    tmp->lChild = NULL;
+    tmp->rChild = NULL;
+
+    (*T) = tmp;
+    return true;
+
+  }
+
+  BST_Node * ptr = (*T); // 辅助指针
+
+  while (ptr->data != key) {
+
+    if (key < ptr->data) {
+
+      if (ptr->lChild != NULL) {
+        ptr = ptr->lChild;
+      } else {
+        // 插入新结点到 lChild
+        BST_Node * tmp = (BST_Node *)malloc(sizeof(BST_Node));
+        if (tmp == NULL) {
+          return false;
+        }
+
+        tmp->data = key;
+        tmp->lChild = NULL;
+        tmp->rChild = NULL;
+
+        ptr->lChild = tmp;
+        return true;
+      }
+
+    } else {
+
+      if (ptr->rChild != NULL) {
+        ptr = ptr->rChild;
+      } else {
+        // 插入新结点到 rChild
+        BST_Node * tmp = (BST_Node *)malloc(sizeof(BST_Node));
+        if (tmp == NULL) {
+          return false;
+        }
+
+        tmp->data = key;
+        tmp->lChild = NULL;
+        tmp->rChild = NULL;
+
+        ptr->rChild = tmp;
+        return true;
+      }
+
+    }
+
+  }
+
+  // 结点的关键字 == key，拒绝插入
+  return false;
 
 }
 
