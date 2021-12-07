5f400f5b5ca65e2c62d8bc432f0a6deb234de4a2
Brannua <374453156@qq.com>
2021-12-07 21:32:14 +0800

    update

--- 专业课真题整理/简易计算器.c
+++ 专业课真题整理/简易计算器.c
@@ -6,18 +6,33 @@
 
 #include <stdio.h>
 
-int main(void) {
+void _prompt() {
   printf("=========欢迎使用简易计算器=========\n");
   printf("0 ----> exit\n");
   printf("1 ----> +\n");
   printf("2 ----> -\n");
   printf("3 ----> *\n");
   printf("4 ----> /\n");
+}
+
+void count(int curOp, float num1, float num2) {
+  float res = 0.0;
+  switch (curOp) {
+    case 1: res = num1 + num2; break;
+    case 2: res = num1 - num2; break;
+    case 3: res = num1 * num2; break;
+    case 4: res = num1 / num2; break;
+    default: break;
+  }
+  printf("result=%f\n", res);
+}
+
+int main(void) {
+  _prompt();
 
   int curOp;
   float num1 = 0.0,
-        num2 = 0.0,
-        res = 0.0;
+        num2 = 0.0;
 
   while (1) {
     printf("请选择你想进行的操作：");
@@ -28,15 +43,7 @@ int main(void) {
 
     printf("请输入两个操作数，用逗号分隔，按回车确认：");
     scanf("%f,%f", &num1, &num2);
-    switch (curOp) {
-      case 1: res = num1 + num2; break;
-      case 2: res = num1 - num2; break;
-      case 3: res = num1 * num2; break;
-      case 4: res = num1 / num2; break;
-      default: break;
-    }
-
-    printf("result=%f\n", res);
+    count(curOp, num1, num2);
   }
 
   printf("Bye~\n");
