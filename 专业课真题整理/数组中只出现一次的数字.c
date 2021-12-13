7d412524ea55f22aee48ca3da656165e03ba3a8f
Brannua <374453156@qq.com>
2021-12-13 11:54:25 +0800

    update

---
+++ 专业课真题整理/数组中只出现一次的数字.c
@@ -0,0 +1,21 @@
+/*
+  https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/
+  在一个数组中除一个数字只出现一次之外，其他数字都出现了三次，请找出那个只出现一次的数字
+*/
+
+int findOne(int arr[], int arrLen) {
+
+  // from cpp-library <algorithm> which is basing on quick_sort fn.
+  sort(arr, arr + arrLen);
+
+  int resIdx = 0;
+
+  while (1) {
+    if (resIdx + 2 < arrLen && arr[resIdx + 2] != arr[resIdx]) {
+      return arr[resIdx];
+    } else {
+      resIdx += 3;
+    }
+  }
+
+}
