8873dfd5ac17ec4846c40af39cac7f9019cb8d4b
lpj <374453156@qq.com>
2022-01-10 17:53:04 +0800

    update bubbleSort.c

--- src/9_sort/bubbleSort.c
+++ src/9_sort/bubbleSort.c
@@ -1,52 +1,62 @@
 /**
  * 冒泡排序（基于交换的排序）
+ * 一趟冒泡（会将乱序部分的最大关键字冒泡到乱序序列的最右端，排好序的部分就不用再扫描了）
  */
 
 #include <stdio.h>
 
-void _printArr(int arr[], int arrLen) {
-  for (int i = 0; i < arrLen; i ++) {
-    printf("%d ", arr[i]);
+void _printArr(int arr[], int begin, int end);
+void _swap(int arr[], int a, int b);
+
+void bubbleSort(int arr[], int begin, int end) {
+
+  // 数组中至少得有俩元素，你才能排序
+  if (begin >= end) {
+    return;
   }
-  printf("\n");
-}
 
-void _swap(int * a, int * b) {;
-  int tmp = *b;
-  *b = *a;
-  *a = tmp;
-}
+  // 控制冒泡趟数
+  for (int i = begin; i < end; i ++) {
 
-void bubbleSort(int arr[], int arrLen) {
-  if (arrLen < 2) { return; }
+    int _flag = 1;
 
-  // 控制冒泡的趟数
-  for (int i = 0; i < arrLen - 1; i ++) {
-
-    int _flag = 0;
-
-    // 一趟冒泡（会将乱序部分的最大关键字冒泡到乱序序列的最右端，排好序的部分就不用再扫描了）
-    for (int j = 1; j < arrLen - i; j ++) {
-
-      // 保障排序算法的稳定性
-      if (arr[j] < arr[j - 1]) {
-        _swap(&arr[j], &arr[j - 1]);
-
-        if (_flag == 0) { _flag = 1; }
+    // 一趟冒泡
+    for (int j = begin; j < end - i; j ++) { // 3. end - i 是因为只需对还未排序的子序列进行排序
+      // 1. 保障排序算法的稳定性
+      if (arr[j] > arr[j + 1]) {
+        _swap(arr, j, j + 1);
+        _flag = 0;
       }
-
     }
 
-    if (_flag == 0) { break; }
+    // 2. 如果在一趟冒泡中没有发生元素交换，说明已经排好序，结束即可
+    if (_flag) {
+      break;
+    }
 
   }
 }
 
 int main() {
   int arr[] = { 2, 4, 1, 6, -1, 8, 0, 2 },
-      arrlen = sizeof(arr) / sizeof(int);
+      arrLen = sizeof(arr) / sizeof(int);
 
-  bubbleSort(arr, arrlen);
-  _printArr(arr, arrlen);
+  bubbleSort(arr, 0, arrLen - 1);
+  _printArr(arr, 0, arrLen - 1);
   return 0;
+}
+
+/*-- Utils are under below. --*/
+
+void _swap(int arr[], int a, int b) {
+  int tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
+}
+
+void _printArr(int arr[], int begin, int end) {
+  for (int i = begin; i <= end; i ++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
 }
