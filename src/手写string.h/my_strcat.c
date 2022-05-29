fcb69a98517afe33bd63b11df74a9af52f5b5a0b
$UID: 0 <374453156@qq.com>
2022-05-29 09:43:19 +0800

    Update my_strcat.c

--- src/手写string.h/my_strcat.c
+++ src/手写string.h/my_strcat.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * 函数原型:      char * strcat(char *restrict s1, const char *restrict s2);
+ * 用途:         拷贝一份 s2 指向的字符串，将其拼接至 s1 指向的字符串的后面
+ * 期望返回值:    指针 s1，故函数返回值类型为 char *
+ * 注意:         restrict 关键字，用于限制 s1 和 s2 所指向的字符串不能有重叠（from C99）
+ */
+
 char * my_strcat(char *restrict destStr, const char *restrict srcStr) {
 
   // char * tmp = destStr;
